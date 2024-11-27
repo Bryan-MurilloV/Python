e1a6032c01cc8034e819bdd0a7055e7c29bfcea5
Bryan Murillo <bryanmurillo131206@gmail.com>
2024-11-27 14:48:33 -0600

    Update 29-bodega-vino.py
    Change the name of arrays

--- 29-bodega-vino.py
+++ 29-bodega-vino.py
@@ -1,5 +1,5 @@
-vino2 = []
-vino3 = []
+lista_vino2 = []
+lista_vino3 = []
 subtotalvino = 0
 totalvino = 0
 i = 'Si'
@@ -7,13 +7,13 @@ año = 0
 while i == 'Si':
     año += 1
     vino1 = int(input(f"Ingrese lo producido del vino 1 en el año {año}\n"))
-    wine2 = int(input(f"Ingrese lo producido del vino 2 en el año {año}\n"))
-    vino2.append(wine2)
-    wine3 = int(input(f"Ingrese lo producido del vino 3 en el año {año}\n"))
-    vino3.append(wine3)
+    vino2 = int(input(f"Ingrese lo producido del vino 2 en el año {año}\n"))
+    lista_vino2.append(vino2)
+    vino3 = int(input(f"Ingrese lo producido del vino 3 en el año {año}\n"))
+    lista_vino3.append(vino3)
     vino4 = int(input(f"Ingrese lo producido del vino 4 en el año {año}\n"))
     vino5 = int(input(f"Ingrese lo producido del vino 5 en el año {año}\n"))
-    subtotalvino = vino1 + wine2 + wine3 + vino4 + vino5
+    subtotalvino = vino1 + vino2 + vino3 + vino4 + vino5
     totalvino = totalvino + subtotalvino
     while True:
         i = input("¿Nuevo Año?\n").capitalize().strip()
@@ -21,12 +21,12 @@ while i == 'Si':
             break
         else:
             print("Solo \"Si\" o \"No\"")
-vin = sorted(vino2)
-añovin2 = vino2.index(vin[-1])+1
+vin = sorted(lista_vino2)
+añovin2 = lista_vino2.index(vin[-1])+1
 print(f"El año con mas Vino del tipo 2 fue el año {
       añovin2} con {vin[-1]} Lts.")
-if 0 in vino3:
-    print(f"El año {vino3.index(0)+1} no se produjo Vino del tipo 3.")
+if 0 in lista_vino3:
+    print(f"El año {lista_vino3.index(0)+1} no se produjo Vino del tipo 3.")
 else:
     print("No hubo años con nula produccion del Vino tipo 3")
 print(f"El total de Vino en {año} años fue de {totalvino} Lts.")
