9e09a3a29ac43ed2c197df499f511c5319d21d8b
Bryan Murillo <bryanmurillo131206@gmail.com>
2025-02-01 13:09:21 -0600

    Update 37-decimal-binario.py

--- 37-decimal-binario.py
+++ 37-decimal-binario.py
@@ -8,5 +8,9 @@ def convertidor(decimal):
 
 
 decimal = int(input("Ingresa el numero a convertir a binario:\n"))
+
+while (decimal < 0):
+    decimal = int(input("Solo numeros positivos:\n"))
+
 binario = convertidor(decimal)
 print(f"El {decimal} en binario es: {binario[::-1]}\n")
